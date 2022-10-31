1a5e5f5e12617f9caf7c4961194899f14d2faab8
Vladimir Leonov <vow.leonoff@yandex.ru>
2022-10-31 16:05:08 +0300

    add one catalog item

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,3 @@
-/* $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-      });
-  }); */
-
   const slider = tns({
     container: '.carousel__inner',
     items: 1,
