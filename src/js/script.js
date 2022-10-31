e37bdd44aa384e5fb3eb5098d9bebd921a070f58
Vladimir Leonov <vow.leonoff@yandex.ru>
2022-10-31 14:01:00 +0300

    Add carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
         adaptiveHeight: false,
@@ -16,4 +16,20 @@ $(document).ready(function(){
             }
         ]
       });
+  }); */
+
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
   });
